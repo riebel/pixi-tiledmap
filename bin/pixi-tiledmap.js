66c83f5754ea150e2bd2872a16061874da23fc30
Hagen Sommerkorn <hagen.sommerkorn@gmail.com>
2015-06-23 22:26:01 +0200

    updated builds

--- bin/pixi-tiledmap.js
+++ bin/pixi-tiledmap.js
@@ -106,19 +106,12 @@ var Tile = function ( options ) {
 	this.flippedHorizontally = options.flippedHorizontally;
 	this.flippedDiagonally = options.flippedDiagonally;
 
-	if ( this.flippedHorizontally ) {
+	if ( this.flippedHorizontally || this.flippedDiagonally ) {
 		this.scale.x = -1;
 		this.anchor.x = 1;
 	}
 
-	if ( this.flippedVertically ) {
-		this.scale.y = -1;
-		this.anchor.y = 1;
-	}
-
-	if ( this.flippedDiagonally ) {
-		this.scale.x = -1;
-		this.anchor.x = 1;
+	if ( this.flippedVertically || this.flippedDiagonally ) {
 		this.scale.y = -1;
 		this.anchor.y = 1;
 	}
