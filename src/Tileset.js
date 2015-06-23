8d1fa6869ed54a374b88d0f3e8d018a28ce74b78
Hagen Sommerkorn <hagen.sommerkorn@gmail.com>
2015-06-23 22:07:23 +0200

    removed old comments

--- src/Tileset.js
+++ src/Tileset.js
@@ -20,37 +20,6 @@ var Tileset = function ( options ) {
 			this.textures.push( new PIXI.Texture( this.baseTexture, new PIXI.Rectangle( x, y, this.tileWidth, this.tileHeight ) ) );
 		}
 	}
-
-	//var id,
-	//	i,
-	//	p,
-	//	tile,
-	//	shapeData,
-	//	shapes,
-	//	shape,
-	//	points;
-	//for (id in tilesetData.tiles) {
-	//	tile = tilesetData.tiles[id];
-	//	for (i = 0; i < tile.objectgroup.objects.length; i++) {
-	//		shapeData = tile.objectgroup.objects[0];
-	//		shapes = [];
-	//		if (shapeData.polygon) {
-	//			points = [];
-	//			for (p = 0; p < shapeData.polygon.length; p++) {
-	//				points.push(shapeData.polygon[p].x);
-	//				points.push(shapeData.polygon[p].y);
-	//			}
-	//			shape = new PIXI.Polygon(points);
-	//		} else if (shapeData.ellipse) {
-	//			shape = new PIXI.Circle(shapeData.x, shapeData.y, shapeData.height / 2);
-	//		} else {
-	//			shape = new PIXI.Rectangle(shapeData.x, shapeData.y, shapeData.width, shapeData.height);
-	//		}
-	//		shapes.push(shape);
-	//	}
-	//	tileset.tiles[+id + 1] = {collision: shapes};
-	//}
-
 };
 
 module.exports = PIXI.extras.TileSet = Tileset;
