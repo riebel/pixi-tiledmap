a4a63b88949f37e5d99c153bcb5509713367c3fb
Hagen Sommerkorn <hagen.sommerkorn@gmail.com>
2017-08-27 00:09:44 +0200

    added example with webpack

---
+++ example/webpack.config.js
@@ -0,0 +1,14 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [new HtmlWebpackPlugin()],
+    node: {
+        fs: "empty"
+    }
+};
